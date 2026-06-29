c29b4e37660192ab80b8147eb4f26507b8712e21
Henry Cho <hyunmook@qti.qualcomm.com>
2026-06-29 13:17:57 +0900

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,7 @@
 alias vi='nvim'
 alias qg='qgenie'
 alias bat='batcat'
-
+alias codex='qgenie'
 
 alias ls='eza -lg --icons --git --group-directories-last --color=always --sort=name'
 alias la='eza -alg --icons --git --group-directories-last --color=always --sort=name'
@@ -11,4 +11,3 @@ alias ld='eza -lgD --icons --git --color=always'
 alias lf='eza -lfg --icons --git --color=always'
 alias lh='eza -dlg .* --group-directories-first --icons --git'
 alias lt='eza -alg --icons --git --color=always --sort=modified'
-
